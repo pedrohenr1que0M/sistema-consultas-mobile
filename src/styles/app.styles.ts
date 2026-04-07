79e51629a6e3062f0c2abf501aded985fc872d35
pedrohenr1que0M <phom.11.2018@gmail.com>
2026-04-07 02:32:25 -0300

    Refatorando o componente App movendo a lógica de consulta para a tela inicial e criando estilos separados para o ConsultaCard

---
+++ src/styles/app.styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#79059C",
+    },
+    scrollContent: {
+        padding: 20,
+        paddingTop: 60,
+    },
+    header: {
+        alignItems: "center",
+        marginBottom: 24,
+    },
+    titulo: {
+        fontSize: 28,
+        color: "#fff",
+        marginBottom: 8,
+    },
+    subtitulo: {
+        fontSize: 18,
+        color: "#fff",
+        opacity: 0.9,
+    },
+});
