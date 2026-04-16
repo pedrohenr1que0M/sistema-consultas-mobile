2dbbc53c4cd00c2b56cd1f92b0ec0556595a6f62
pedrohenr1que0M <phom.11.2018@gmail.com>
2026-04-16 20:53:51 -0300

    Arrays no AsyncStorage, Service Layer e React Navigation

--- src/styles/app.styles.ts
+++ src/styles/app.styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#79059C",
+        backgroundColor: "#BDBDBD",
     },
     scrollContent: {
         padding: 20,
